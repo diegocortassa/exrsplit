79148ac513e424acabd8d5a33a6ad17d7d8255b8
Diego Cortassa <diego@cortassa.net>
2017-09-12 12:58:39 +0200

    fixing tests

--- tests/cmdargs.py
+++ tests/cmdargs.py
@@ -1,3 +1,3 @@
 import collections
 
-CmdArgs = collections.namedtuple('CmdArgs', ['split_channels', 'merge', 'image', 'list'])
+CmdArgs = collections.namedtuple('CmdArgs', ['split_channels', 'merge', 'image', 'list', 'layer'])
